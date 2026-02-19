7c11dbfef209ea629b1f972684db07fbfaa12cb7
Wagner <seuemail@gmail.com>
2026-02-19 20:17:10 -0300

    novo metodo adicionado

---
+++ src/test/java/br/com/senac/exercicio_api/ExercicioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senac.exercicio_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
